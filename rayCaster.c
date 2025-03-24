39ce9b79af3d10682e9d8de9fb1cc2b631fb47cc
WeaverArtiz <spawlo112@gmail.com>
2025-03-24 22:05:26 +0000

    added raycasting, rotation, moving and map

--- rayCaster.c
+++ rayCaster.c
@@ -1,8 +1,12 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
+#include <math.h>
+#include <stdatomic.h>
+#include <stdio.h>
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* Renderer = NULL;
@@ -41,40 +45,80 @@ void end(void){
 //--------------------------------------------------------------------------
 #define MAP_WIDTH 16
 #define MAP_HEIGHT 8
-
-int WorldMap[MAP_HEIGHT][MAP_WIDTH] = {
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,1,0,0,1,1,0,0,1},
-  {1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,1},
-  {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+#define RENDER_DISTANCE 16.0f
+#define MOVE_SPEED 0.3f
+bool WorldMap[MAP_HEIGHT][MAP_WIDTH] = {
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, // x  0
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
-
-struct entity{
-  double posX, posY;
-  double dirX, dirY;
-  double planeX, planeY;
-};
+};//
+// 0 y
+float posX = 4, posY = 4;
+float angle = 0, fov = 3.141592 / 4.0;
 
 void renderFrame(){
-  SDL_SetRenderDrawColor(Renderer, 80, 120, 250, 255);
+  SDL_SetRenderDrawColor(Renderer, 0, 20, 100, 255);
   SDL_RenderClear(Renderer);
 
-  SDL_SetRenderDrawColor(Renderer, 100, 200, 100, 255);
+  SDL_SetRenderDrawColor(Renderer, 20, 100, 30, 255);
   SDL_FRect floor = {0, SCREEN_HEIGHT / 2.0f, (float)SCREEN_WIDTH, SCREEN_HEIGHT / 2.0f};
   SDL_RenderFillRect(Renderer, &floor);
   
   for(int i=0; i < SCREEN_WIDTH; i++){
+    float rayAngle = (angle - fov / 2.0f) + ((float)i / (float)SCREEN_WIDTH) * fov;
+    float rayDistance = 0.01f;
+    bool rayHit = false;
+    
+    float planeX = sinf(rayAngle);
+    float planeY = cosf(rayAngle);
+    
+    while(!rayHit && rayDistance < RENDER_DISTANCE){
+      rayDistance += 0.01f;
 
+      int rayX = (int)(posX + rayDistance * planeX);
+      int rayY = (int)(posY + rayDistance * planeY);
+      
+      if(rayX < 0 || rayX > MAP_WIDTH || rayY < 0 || rayY > MAP_WIDTH){
+        rayHit = true;
+        }
+
+      else if(WorldMap[rayX][rayY] == 1) rayHit = true;
+    
+    }
+    int dCeiling = (int)(SCREEN_HEIGHT/2.0f + SCREEN_HEIGHT / (rayDistance + 1.5f)); //Ceiling distance
+    int dFloor = SCREEN_HEIGHT - dCeiling; //Floor distance
+    
+    SDL_SetRenderDrawColor(Renderer, 100, 30, 30, 255);
+    SDL_RenderLine(Renderer, i, dFloor, i, dCeiling);
   }
 
   SDL_RenderPresent(Renderer);
 }
-
-
+void moveF(){
+  float planeX = sinf(angle);
+  float planeY = cosf(angle);
+  posX += MOVE_SPEED * planeX;
+  posY += MOVE_SPEED * planeY;
+  if(WorldMap[(int)posX][(int)posY]==1){
+    posX -= MOVE_SPEED * planeX;
+    posY -= MOVE_SPEED * planeY;
+  }
+}
+void moveB(){
+  float planeX = sinf(angle);
+  float planeY = cosf(angle);
+  posX -= MOVE_SPEED * planeX;
+  posY -= MOVE_SPEED * planeY;
+  if(WorldMap[(int)posX][(int)posY]==1){
+    posX += MOVE_SPEED * planeX;
+    posY += MOVE_SPEED * planeY;
+  }
+}
 void loop(){
 bool quit = false;
 SDL_Event e;
@@ -82,8 +126,23 @@ SDL_zero(e);
 
 while(!quit){
   while( SDL_PollEvent( &e ) ) {
-  if( e.type == SDL_EVENT_QUIT ) quit = true;
+  if(e.type == SDL_EVENT_QUIT) quit = true;
+  if(e.type == SDL_EVENT_KEY_DOWN) switch(e.key.key){
+        case SDLK_D:
+          angle+=0.03;
+          break;
+        case SDLK_A:
+          angle-=0.03;
+          break;
+        case SDLK_W:
+          moveF();
+          break;
+        case SDLK_S:
+          moveB();
+          break;
+      }
   }
+
   renderFrame();
  
   SDL_UpdateWindowSurface(gWindow);
